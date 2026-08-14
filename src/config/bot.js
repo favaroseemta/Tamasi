3685a72e585e1780465bc02d41c1582469287233
favaroseemta <favaroseemta@gmail.com>
2026-08-14 12:21:14 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -23,7 +23,7 @@ export const botConfig = {
     // 5 = Competing
     activities: [
       {
-        name: "Custom Status", // required by Discord API, not shown in the client
+        name: "Targoncázok", // required by Discord API, not shown in the client
         state: "Targoncázok",     // this is what people actually see
         type: 5,               // Custom
       },
